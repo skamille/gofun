74530232322524fb866c59a4b40295f83f2f11d5
Camille Fournier <camille@apache.org>
2014-01-02 21:01:57 -0500

    more tutorial

--- wiki.go
+++ wiki.go
@@ -29,11 +29,19 @@ func loadPage(title string) (*Page, error) {
 func handler(w http.ResponseWriter, r *http.Request) {
      fmt.Fprintf(w, "Hi there, I love %s", r.URL.Path[1:])
 }
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+     title := r.URL.Path[len("/view/"):]
+     p, _ := loadPage(title)
+     fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+}
+
 func main() {
 	p1 := &Page{Title: "TestPage", Body: []byte("This is a test moo cow")}
 	p1.save()
 	p2, _ := loadPage("TestPage")
 	fmt.Println(string(p2.Body))
 	http.HandleFunc("/", handler)
+	http.HandleFunc("/view/", viewHandler)
 	http.ListenAndServe(":8080", nil)
 }
